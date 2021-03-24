8a700d1d56a746e26674439c2ff0e414d2f43407
manish-coditas <76940863+manish-coditas@users.noreply.github.com>
2021-03-24 18:26:29 +0530

    Update script.sh

--- Jenkins_Terraform/script.sh
+++ Jenkins_Terraform/script.sh
@@ -15,3 +15,9 @@ sudo apt-get install jenkins -y
 sudo apt-get update
 
 sudo apt-get install awscli -y
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update && sudo apt-get install terraform
